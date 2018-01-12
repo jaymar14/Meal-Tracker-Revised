564e394f25d2c38aac65ed30140c277f9e1ded09
jaymar14 <jay.martinez7@yahoo.com>
2018-01-11 23:59:52 -0600

    java files added

---
+++ Food.java
@@ -0,0 +1,50 @@
+package com.mealtracker;
+
+import java.util.ArrayList;
+
+class Food{
+	private final String mName;
+	private final Category mCategory;
+
+	private final double mCalories;
+	private final double mCarbs; //make sure carbs....
+	private final double mFat;
+	private final double mProtein;
+
+	private final ArrayList<Category>mCategories = new ArrayList<>();
+
+	Food(String name, double carbs, double fat, double protein, Category category){
+		this.mName = name;
+		this.mCategory = category;
+		this.mCarbs = carbs;
+		this.mFat = fat;
+		this.mProtein = protein;
+		this.mCalories = calculateCalories(carbs, fat, protein);
+	}
+
+	private static double calculateCalories(double carbs, double fat, double protein){
+		return 9 * fat + 4 * carbs + 4 * protein;
+	}
+
+	double getCalories(){
+		return this.mCalories;
+	}
+	double getCarbs(){
+		return this.mCarbs;
+	}
+	Category getCategory(){
+		return this.mCategory;
+	}
+	double getFat(){
+		return this.mFat;
+	}
+	double getProtein(){
+		return this.mProtein;
+	}
+	String getName(){
+		return this.mName;
+	}
+	void addCategory (Category category){
+		this.mCategories.add(category);
+	}
+}
